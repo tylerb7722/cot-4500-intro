a0f5c602010380304038b17f0fe9e894ab390feb
Tyler Boudreau <122648013+tylerb7722@users.noreply.github.com>
2023-04-24 21:41:28 -0400

    Update intro_to_python.py

--- src/main/intro_to_python.py
+++ src/main/intro_to_python.py
@@ -1,24 +1,32 @@
-# Created by Tyler Boudreau, UCF, COT 4500
+# Tyler Boudreau
+# COT 4500
 import numpy as np
 
-arr = np.empty(shape=(3,3))
+def Reduced_Row_E():
+    arr = np.empty(shape=(3,3))
+    for i in range(arr.shape[0]):
+        for j in range(arr.shape[1]):
+            if i == j:
+                arr[j][i] = 1
+            else:
+                arr[j][i] = 0
+    print(arr, "\n")
 
-for i in range(arr.shape[0]):
-    for j in range(arr.shape[1]):
-        if i == j:
-            arr[j][i] = 1
-        else:
-            arr[j][i] = 0
+def Matrix_of_Three():
+    arr = np.empty(shape=(3,3))
+    for i in range(arr.shape[0]):
+        for j in range(arr.shape[1]):
+            if i != j:
+                arr[j][i] = 3
+    print(arr,"\n")
+    arr = np.delete(arr, 2, 1)
+    print(arr)
 
-print(arr)
-print("\n")
-for i in range(arr.shape[0]):
-    for j in range(arr.shape[1]):
-        if i != j:
-            arr[j][i] = 3
 
-print(arr)
-print("\n")
-arr = np.delete(arr, 2, 1)
+def main():
+    Reduced_Row_E()
+    Matrix_of_Three()
 
-print(arr)
+
+if __name__ == "__main__":
+    main()
